6d806aa2f190d9f868e0ad54bae1b1c5a6e39ca4
Pablo Carceller <pablocarceller@me.com>
2020-02-21 14:59:57 +0100

    Fix result

--- routes/profile.js
+++ routes/profile.js
@@ -2,16 +2,13 @@ const express = require('express');
 const router = express.Router();
 const ensureLogin = require("connect-ensure-login");
 const User = require("../models/User");
-
-
 router.get('/', (req, res, next) => {
     User.findById(req.user._id)
+        .populate("school")
         .then(user => {
             res.render("profile", user)
         })
-
 })
-
 router.get('/:userID', (req, res, next) => {
     User.findById(req.params.userID)
         .populate("user")
@@ -20,9 +17,4 @@ router.get('/:userID', (req, res, next) => {
             res.render("profile", user)
         })
 })
-
-
-
-
-
 module.exports = router;
